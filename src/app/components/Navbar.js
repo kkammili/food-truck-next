cba22b6198a545f9aee5d8f6fb2ee0b06dc5aee5
kkammili <kkrajus777@gmail.com>
2024-07-20 18:47:06 -0500

    Removed unused import of Link in Navbar component.

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import '../styles/_navbar.scss';
 import Image from "next/image";
-import Link from 'next/link';
 import ClientNavbar from './ClientNavbar';
 
 const Navbar = () => {
