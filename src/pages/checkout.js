1cee703555d3c52b6e3f2b21041cc02f3b6fa678
kkammili <kkrajus777@gmail.com>
2024-07-21 13:36:45 -0500

    Updated the Stripe publishable key in the checkout page.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,7 +3,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from '../components/CheckoutForm';
 
-const stripePromise = loadStripe('your-publishable-key-here');
+const stripePromise = loadStripe('pk_test_51Pf4OkA9zrLlfrVWfGsEW13ftQYf5jPxdScKYGaivyHGhrUyY7BjMAvQSoEevdvtq40omPQM6QJuEBNgFQWuAUkk00r9CbzzqG');
 
 const Checkout = () => {
   return (
