633ae7d613ca6b78f236599ab112c1c69e62fa4f
kkammili <kkrajus777@gmail.com>
2024-07-21 12:22:59 -0500

    Implemented conditional rendering for the Checkout component in the main app layout.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,18 @@ import { Providers } from '../app/Provider';
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+    const { pathname } = useRouter();
+
     return (
+      <>
+        {pathname === '/checkout' ? (
+          <Checkout />
+        ) : (
+          <Providers>
+            <Component {...pageProps} />
+          </Providers>
+        )}
+      </>
       <Providers>
         <Component {...pageProps} />
       </Providers>
