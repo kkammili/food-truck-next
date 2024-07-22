3070bfbba3a0f65efae86e3afb5adb4923fb6992
kkammili <kkrajus777@gmail.com>
2024-07-22 09:29:12 -0500

    feat(stripe): small stripe fixes

--- src/pages/api/create-payment-intent.js
+++ src/pages/api/create-payment-intent.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "eur",
+    currency: "usd",
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
