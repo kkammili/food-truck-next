271b0f542fec2deab8ecb7ad382f8f5f221fa585
kkammili <kkrajus777@gmail.com>
2024-07-22 09:04:52 -0500

    feat(Stripe): stripe impl layout done

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from '../components/CheckoutForm';
-import './checkout.css';
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe('pk_test_51Pf4OkA9zrLlfrVWfGsEW13ftQYf5jPxdScKYGaivyHGhrUyY7BjMAvQSoEevdvtq40omPQM6QJuEBNgFQWuAUkk00r9CbzzqG');
+import Payments from "../app/components/Payments";
+import "../app/styles/_checkout.scss"
 
-const Checkout = () => {
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export default function App() {
+  const [clientSecret, setClientSecret] = React.useState("");
+
+  React.useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
-    <div className="checkout-container">
-      <div className="checkout-form">
-        <h2>Checkout</h2>
-        <Elements stripe={stripePromise}>
-          <CheckoutForm />
+    <div className="payment-container">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <Payments />
         </Elements>
-      </div>
+      )}
     </div>
   );
-};
-
-export default Checkout;
+}
