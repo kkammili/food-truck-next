7968dc772aed0c1d025edad37c589cc65142ad9c
kkammili <kkrajus777@gmail.com>
2025-06-15 11:51:55 -0500

    feat: add ToastContainer for notifications in RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
@@ -6,6 +6,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 import { Providers } from "./Provider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "Restaraunt Name",
@@ -15,19 +17,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Food Truck</title>
-    </Head>
-    <body className={inter.class}>
-      <Providers>
-
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-      </Providers>
-
-    </body>
-  </html>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Food Truck</title>
+      </Head>
+      <body className={inter.class}>
+        <Providers>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+            transition="Bounce"
+          />
+        </Providers>
+      </body>
+    </html>
   );
 }
